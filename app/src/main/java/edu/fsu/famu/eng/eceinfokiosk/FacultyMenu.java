8e7c58106a74e3a6a7eb0c7762f984620d7c8942
mad-macks <macklintweedie@gmail.com>
2018-03-21 12:49:40 -0400

    Fifth Draft

--- app/src/main/java/edu/fsu/famu/eng/eceinfokiosk/FacultyMenu.java
+++ app/src/main/java/edu/fsu/famu/eng/eceinfokiosk/FacultyMenu.java
@@ -1,7 +1,9 @@
 package edu.fsu.famu.eng.eceinfokiosk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FacultyMenu extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class FacultyMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_menu);
+    }
+
+    public void toMain(View view) {
+        //call main activity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void toChem(View view) {
+        //go to Chem faculty directory
+        Intent intent = new Intent(this, Chem.class);
+        startActivity(intent);
     }
 }

1d38d0600a00f33c69494bc4887459c8f1057162
mad-macks <macklintweedie@gmail.com>
2018-04-19 15:20:00 -0400

    Started adding elec professors

---
+++ app/src/main/java/edu/fsu/famu/eng/eceinfokiosk/Andrei.java
@@ -0,0 +1,21 @@
+package edu.fsu.famu.eng.eceinfokiosk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Andrei extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_andrei);
+    }
+
+    public void toElec(View view) {
+        //go to Electrical faculty directory
+        Intent intent = new Intent(this, Elec.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/edu/fsu/famu/eng/eceinfokiosk/Elec.java
+++ app/src/main/java/edu/fsu/famu/eng/eceinfokiosk/Elec.java
@@ -1,7 +1,9 @@
 package edu.fsu.famu.eng.eceinfokiosk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Elec extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class Elec extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elec);
+    }
+
+    //go to page for andrei
+    public void viewAndrei(View view) {
+        //call new activity
+        Intent intent = new Intent(this,Andrei.class);
+        startActivity(intent);
+    }
+
+    //go to page for harvey
+    public void viewHarvey(View view) {
+        //call new activity
+        Intent intent = new Intent(this,Harvey.class);
+        startActivity(intent);
+    }
+
+    //go back to faculty page
+    public void viewFaculty(View view) {
+        //call new activity
+        Intent intent = new Intent(this, FacultyMenu.class);
+        startActivity(intent);
     }
 }
